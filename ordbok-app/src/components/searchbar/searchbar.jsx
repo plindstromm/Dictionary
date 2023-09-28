c5dfbcff43b93b5fb6c9a5186ee594b8ae2e53c4
plindstromm <mukkepp@gmail.com>
2023-09-28 14:59:53 +0100

    synonym add + test

--- ordbok-app/src/components/searchbar/searchbar.jsx
+++ ordbok-app/src/components/searchbar/searchbar.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import './searchbar.css';
 
+// Jag vet att detta inte är sättet man borde strukturera upp ett projekt med väldigt få komponenter, men jag siktar bara på G så jag hoppas det är ok.
+
+
 function Searchbar() {
-  // States för varje elememnt
+  // States för varje element
   const [word, setWord] = useState('');
+  const [clickedWord, setClickedWord] = useState('');
   const [definition, setDefinition] = useState(null);
   const [audioUrl, setAudioUrl] = useState(null);
   const [phonetic, setPhonetic] = useState(null);
-  const [example, setExample] = useState(null); 
-  const [meaning, setMeaning] = useState(null); // Added state for Meaning
+  const [example, setExample] = useState(null);
+  const [meaning, setMeaning] = useState(null);
+  const [synonyms, setSynonyms] = useState(null);
   const [error, setError] = useState(null);
 
   // API fetch sker på klick
@@ -17,12 +22,15 @@ function Searchbar() {
     setDefinition(null);
     setAudioUrl(null);
     setPhonetic(null);
-    setExample(null); 
-    setMeaning(null); // Reset Meaning
+    setExample(null);
+    setMeaning(null);
+    setSynonyms(null);
     setError(null);
 
-    //fetch sker endas om ett ord matats in i inputfäletet
+    // fetch sker endast om ett ord matats in i inputfältet
     if (word) {
+      setClickedWord(word); // Set the clicked word here
+      
       const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
       try {
         const response = await fetch(url);
@@ -32,14 +40,13 @@ function Searchbar() {
         }
 
         const data = await response.json();
-
         console.log(data)
 
         if (data[0]?.meanings[0]?.definitions[0]?.definition) {
           setDefinition(data[0].meanings[0].definitions[0].definition);
         }
         if (data[0]?.meanings[0]?.definitions[0]?.example) {
-          setExample(data[0].meanings[0].definitions[0].example); 
+          setExample(data[0].meanings[0].definitions[0].example);
         }
         if (data[0]?.phonetics[0]?.audio) {
           setAudioUrl(data[0].phonetics[0].audio);
@@ -48,22 +55,29 @@ function Searchbar() {
           setPhonetic(data[0].phonetics[0].text);
         }
         if (data[0]?.meanings) {
-          setMeaning(data[0].meanings.map(meaning => meaning.partOfSpeech).join(', ')); 
+          const meaningWithSynonyms = data[0].meanings.find(
+            (meaning) => meaning.synonyms && meaning.synonyms.length > 0
+          );
+          if (meaningWithSynonyms) {
+            setSynonyms(meaningWithSynonyms.synonyms);
+          }
+          setMeaning(data[0].meanings.map(meaning => meaning.partOfSpeech).join(', '));
         }
         setError(null);
       } catch (error) {
         setError('Error fetching the API: ' + error.message);
-        setDefinition(null); //resetar på error
+        setDefinition(null);
         setAudioUrl(null);
         setPhonetic(null);
-        setExample(null); 
-        setMeaning(null); //resetar på error
+        setExample(null);
+        setMeaning(null);
+        setSynonyms(null);
       }
     } else {
-      setError('No word entered'); //error om inget ord matats in
+      setError('No word entered');
     }
   };
-
+  // emelenten renderas endast om ett värde finns
   return (
     <div className="searchbar">
       <input
@@ -76,17 +90,20 @@ function Searchbar() {
       <button className='search-button' onClick={fetchWordDefinition}>Fetch Definition</button>
       {error && <p className='error' style={{ color: 'red' }}>{error}</p>}
       <section className='main-section'>
+        {(definition || meaning || phonetic || example || synonyms || audioUrl) && 
+          <h1 className='word-title'>{clickedWord}</h1>}
         {definition && <p className='description'><strong>Definition:</strong> {definition}</p>}
-        {meaning && <p className='meaning'><strong>Meaning:</strong> {meaning}</p>}  
+        {meaning && <p className='meaning'><strong>Meaning:</strong> {meaning}</p>}
         {phonetic && <p className='phonetic'><strong>Phonetic:</strong> {phonetic}</p>}
-        {example && <p className='example'><strong>Example:</strong> {example}</p>}  
+        {example && <p className='example'><strong>Example:</strong> {example}</p>}
+        {synonyms && <p className='synonyms' data-testid="synonyms-container"><strong>Synonyms:</strong> {synonyms.join(', ')}</p>}
         {audioUrl && (
           <div className='audio'>
             <audio controls data-testid="audio-element">
               <source src={audioUrl} type="audio/mpeg" />
               Your browser does not support the audio element.
             </audio>
-          </div> 
+          </div>
         )}
       </section>
     </div>
@@ -94,5 +111,8 @@ function Searchbar() {
 }
 
 export default Searchbar;
+
+
+
 
 
--- ordbok-app/tests/App.test.jsx
+++ ordbok-app/tests/App.test.jsx
@@ -4,35 +4,43 @@ import { rest } from 'msw';
 import Searchbar from '../src/components/searchbar/searchbar';
 import Header from '../src/components/header/header';
 
-// mock response
-const mockResponse = [
+
+const mockResponseFootball = [
   {
-    meanings: [
-      {
-        definitions: [
-          {
-            definition: 'A robot programmed to perform tasks autonomously.',
-            example: 'The bot was programmed to play games.',
-          },
-        ],
-      },
-    ],
+    word: "football",
     phonetics: [
       {
-        text: '/bɒt/',
-        audio: 'https://some-audio-url.com/bot.mp3',
+        text: "[fʷʊʔt̚bɑɫ]",
+        audio: "https://api.dictionaryapi.dev/media/pronunciations/en/football-au.mp3",
+      },
+    ],
+    meanings: [
+      
+      {
+        
+        partOfSpeech: "noun, verb",
+        
+        definitions: [
+          {
+            definition: "(general) A sport played on foot in which teams attempt to get a ball into a goal or zone defended by the other team.",
+            example: "Roman and medieval football matches were more violent than any modern type of football.",
+            
+          },
+        ],
+        synonyms: ['atomic football', 'black bag', 'black box', 'nuclear football'],
       },
     ],
   },
 ];
 
-// setup för att söka efter ordet bot "bot"
 const server = setupServer(
   rest.get(
     "https://api.dictionaryapi.dev/api/v2/entries/en/:word",
     (req, res, ctx) => {
       if (req.params.word === "bot") {
-        return res(ctx.json(mockResponse));
+       
+      } else if (req.params.word === "football") {
+        return res(ctx.json(mockResponseFootball));
       }
       return res(ctx.status(404));
     }
@@ -50,14 +58,23 @@ describe('Header', () => {
     expect(h2Element).toBeInTheDocument();
   });
 
+  it('should render searchbar and header', () => {
+    render(<Header />);
+    render(<Searchbar />);
+  });
+
   describe('Searchbar', () => {
-    it('should render the button and input field', () => {
+    it('should render the button', () => {
+      render(<Searchbar />);
+      
+      const buttonElement = screen.getByText(/Fetch Definition/i);
+      expect(buttonElement).toBeInTheDocument();
+    });
+
+    it('should render the input field', () => {
       render(<Searchbar />);
       const inputElement = screen.getByPlaceholderText(/Enter a word/i);
-      const buttonElement = screen.getByText(/Fetch Definition/i);
-      
       expect(inputElement).toBeInTheDocument();
-      expect(buttonElement).toBeInTheDocument();
     });
 
     it('should type "cool" into the input field and click the button', async () => {
@@ -69,26 +86,37 @@ describe('Header', () => {
       expect(inputElement.value).toBe('cool');
 
       fireEvent.click(buttonElement);
-    
     });
 
-    it('should display the correct information for the word "bot"', async () => {
+   
+
+    it('should display the correct information for the word "football"', async () => {
       render(<Searchbar />);
       
       const inputElement = screen.getByPlaceholderText(/Enter a word/i);
       const buttonElement = screen.getByText(/Fetch Definition/i);
 
-      fireEvent.change(inputElement, { target: { value: 'bot' } });
+      fireEvent.change(inputElement, { target: { value: 'football' } });
       fireEvent.click(buttonElement);
 
-      const definitionElement = await screen.findByText(/A robot programmed to perform tasks autonomously./i);
-      const exampleElement = await screen.findByText(/The bot was programmed to play games./i);
-      const phoneticElement = await screen.findByText(/\/bɒt\//i);
+      
+  
 
+      const definitionElement = await screen.findByText(/\(general\) A sport played on foot in which teams attempt to get a ball into a goal or zone defended by the other team./i);
+      const exampleElement = await screen.findByText(/Roman and medieval football matches were more violent than any modern type of football./i);
+      const phoneticElement = await screen.findByText(/\[fʷʊʔt̚bɑɫ\]/i);
+      const meaningElement = await screen.findByText(/noun, verb/i);
+      const synonymsContainer = await screen.findByTestId('synonyms-container');
+      
+
+
+      expect(meaningElement).toBeInTheDocument();
       expect(definitionElement).toBeInTheDocument();
       expect(exampleElement).toBeInTheDocument();
       expect(phoneticElement).toBeInTheDocument();
-    }); 
+      expect(synonymsContainer).toBeInTheDocument()
+      
+    });
   });
 
   it('should render audio element when audioUrl is available', async () => {
@@ -97,12 +125,13 @@ describe('Header', () => {
     const inputElement = screen.getByPlaceholderText(/Enter a word/i);
     const buttonElement = screen.getByText(/Fetch Definition/i);
 
-    fireEvent.change(inputElement, { target: { value: 'bot' } });
+    fireEvent.change(inputElement, { target: { value: 'football' } });
     fireEvent.click(buttonElement);
 
-    // Using findByTestId to find the audio element
+    
     const audioElement = await screen.findByTestId('audio-element');
     expect(audioElement).toBeInTheDocument();
+    
   });
 });
 
